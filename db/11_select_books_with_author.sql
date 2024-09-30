d46a2b3639c12e6ae43200e38999d100333a5b2b
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-30 21:28:03 +0100

    add working queries for 11_select_books_with_author.sql

--- db/11_select_books_with_author.sql
+++ db/11_select_books_with_author.sql
@@ -2,6 +2,13 @@
 
 \echo '\n Here is a list of book titles with the corresponding author:\n'
 
-
+SELECT books.title, authors.author_name
+FROM books
+JOIN authors on books.author_id = authors.author_id;
 
 \echo '\n Here is a list of authors without an associated book:\n'
+
+SELECT author_name
+FROM authors
+LEFT JOIN books on authors.author_id = books.author_id
+WHERE books.author_id is NULL;
