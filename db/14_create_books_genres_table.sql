cb0fbd900ba707afa4930c4a98072ee049568f7b
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-30 21:55:06 +0100

    create the books_genres junction table

--- db/14_create_books_genres_table.sql
+++ db/14_create_books_genres_table.sql
@@ -1,1 +1,7 @@
 \c test_bookshop
+
+CREATE TABLE books_genres (
+    book_id INTEGER REFERENCES books(book_id),
+    genre_id INTEGER REFERENCES genres(genre_id),
+    PRIMARY KEY (book_id, genre_id)
+);
