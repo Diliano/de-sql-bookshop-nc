77d51f22e766906770583a2dcfdcc710089f614e
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-30 21:31:43 +0100

    insert genre data into the genres table:

--- db/13_insert_genres.sql
+++ db/13_insert_genres.sql
@@ -1,1 +1,14 @@
 \c test_bookshop
+
+INSERT INTO genres
+    (genre_name)
+VALUES
+    ('science fiction'),
+    ('children''s'),
+    ('romance'),
+    ('fantasy'),
+    ('dystopian'),
+    ('science'),
+    ('adventure'),
+    ('classics')
+RETURNING *;
