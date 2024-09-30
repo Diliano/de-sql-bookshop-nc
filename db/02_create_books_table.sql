26202b9952de529669508d1cfcf783faec49cea1
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-30 16:56:54 +0100

    create the books table

--- db/02_create_books_table.sql
+++ db/02_create_books_table.sql
@@ -1,3 +1,12 @@
-\c my_bookshop;
+\c test_bookshop;
 
 -- create the books table here
+
+CREATE TABLE books (
+    book_id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    price_in_pence INTEGER,
+    quantity_in_stock INTEGER,
+    release_date DATE,
+    is_fiction BOOLEAN
+);
