77de0aa8664aadd4ca1f754331f0eb5cb67e00a5
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-01 09:49:28 +0100

    insert book/genre data into the books_genres table

--- db/15_associate_book_genres.sql
+++ db/15_associate_book_genres.sql
@@ -1,1 +1,15 @@
 \c test_bookshop
+
+INSERT INTO books_genres
+    (book_id, genre_id)
+VALUES
+    (1, 1), (1, 7), (1, 8),
+    (2, 2), (2, 4), (2, 8),
+    (3, 2), (3, 8),
+    (4, 3), (4, 8),
+    (5, 5), (5, 1), (5, 8),
+    (6, 5), (6, 1), (6, 8),
+    (7, 1), (7, 7), (7, 8),
+    (10, 3), (10, 8);
+
+SELECT * FROM books_genres;
