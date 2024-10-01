928b70d30d7c5ef43f69b300b4ec34d656f1fd13
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-01 20:58:20 +0100

    add working queries for 16_query_books_with_genres.sql

--- db/16_query_books_with_genres.sql
+++ db/16_query_books_with_genres.sql
@@ -2,6 +2,16 @@
 
 \echo '\n Here are the genres for Emma:\n'
 
-
+SELECT genres.genre_name 
+FROM books_genres
+JOIN books ON books_genres.book_id = books.book_id
+JOIN genres ON books_genres.genre_id = genres.genre_id
+WHERE books.title = 'Emma';
 
 \echo '\n Here are all the dystopian books:\n'
+
+SELECT books.title
+FROM books_genres
+JOIN books ON books_genres.book_id = books.book_id
+JOIN genres ON books_genres.genre_id = genres.genre_id
+WHERE genres.genre_name = 'dystopian';
